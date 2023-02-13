c3601839cacec6b0985eb5710d36803ac8c20a07
ChaRJ099 <charline.ramelet@gmail.com>
2023-02-13 18:41:24 +0100

    delete comments & modif readme.md

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,31 +1,6 @@
 import "./header.scss";
 import { NavLink } from "react-router-dom";
 import Logo from "../../Assets/logo_wealth_health.png";
-// import Nav from "../nav/nav";
-// import { useState } from "react";
-
-// function SetPathToNav() {
-
-//   const [pathPage, setPathPage] = useState();
-//   const [reload, setReload] = useState(false);
-
-//   // // const navigate = useNavigate();
-//   // console.log(window.location)
-//   if (!pathPage && window.location.pathname) {
-//     setReload(!reload);
-
-//     setPathPage(window.location.pathname);
-//   }
-
-//   if (pathPage) {
-//     console.log("pathPage", pathPage)
-//     return pathPage === "/" ? (<Nav path="/employee-list" navText="View current employees" />) : (<Nav path="/" navText="Create new employee" />)
-//   }
-
-
-
-
-// }
 
 function Header({ link, linkName }) {
 
@@ -34,7 +9,6 @@ function Header({ link, linkName }) {
       <div className="header-container">
         <div className="header-title-container">
           <NavLink to="/"><img className="header__logo" src={Logo} alt="logo wealth health" /></NavLink>
-          {/* <span className="header__title">Wealth Health |</span> */}
           <span className="header__title">HRnet</span>
         </div>
         <nav className="header-nav">
